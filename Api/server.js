1dbb5aa3956bf071b1452a4bf24ce0efc02eb158
Jaydeepmalvya02 <jaydeepmalvya02@gmail.com>
2025-05-14 12:46:33 +0530

    deploy commit

--- Api/server.js
+++ Api/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const userRoutes = require("./routes/userRoutes");
 const dotenv=require('dotenv')
-
+const cors=require('cors')
 dotenv.config()
 const app = express();
 
