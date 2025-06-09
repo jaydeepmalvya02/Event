46efeb4674d66387f074c740786b8c47cdfba30b
Jaydeepmalvya02 <jaydeepmalvya02@gmail.com>
2025-06-09 19:14:22 +0530

    correct error

--- Api/controller/event.js
+++ Api/controller/event.js
@@ -3,8 +3,8 @@ const Event = require("../model/eventModel");
 // GET all events
 const getAllEvents = async (req, res) => {
   try {
-    const events = await Event.find();
-    res.status(200).json(events);
+    const event = await Event.find();
+    res.status(200).json(event);
   } catch (err) {
     res.status(500).json({ message: "Failed to fetch events", error: err });
   }
