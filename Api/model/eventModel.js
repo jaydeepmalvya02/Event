9f5c25636209ff056e2469f751419e3513cdac96
Jaydeepmalvya02 <jaydeepmalvya02@gmail.com>
2025-06-10 12:35:10 +0530

    edit event model

--- Api/model/eventModel.js
+++ Api/model/eventModel.js
@@ -18,7 +18,7 @@ const eventSchema = new mongoose.Schema(
       type: String,
       required: true,
       trim: true,
-      match: /^https?:\/\/.+/,
+      
     },
     image: {
       type: String,
